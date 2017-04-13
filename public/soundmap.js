643bc4d90da5ea56b15ce1f172803809df82683f
Sang Won Lee <snaglee@umich.edu>
2017-04-13 00:01:10 -0400

    node module latest

--- public/soundmap.js
+++ public/soundmap.js
@@ -1,28 +1,28 @@
 var soundmap = {
-  'chatter':'chatter_amp.mp3' // chatter that starts in the thrid page.
-  , 'gong' : 'indonesian_gong.wav' // click sound from the 2nd page.
-  , 'tick1' : 'tick1.wav' // click sound from the 2nd page.
-  , 'ir1' : 'ir1.wav' // reverb impulse response.
-  , 'sus1' : 'sus_note.wav' // another reberb response
-  , 'piano1': 'piano_note1_f_sharp.wav' // this is not used
-  , 'reversegate' :'H3000-ReverseGate.mp3'
-  ,'pianoloop2':'pianoloop2.wav'
-  ,'nnote1':'nnote1.wav'
-  ,'pause1':'note_fio.wav'
-  ,'pause2':'nnote1.wav'
-  ,'click':'click.wav'
-  ,'woodangtang':'woodangtang.wav'
-  , 'june_A' : 'june_A.mp3'
-  , 'june_B' : 'june_B.mp3'
-  , 'june_C' : 'june_C.mp3'
-  , 'june_D' : 'june_D.mp3'
-  , 'june_E' : 'june_E.mp3'
-  , 'june_F' : 'june_F.mp3'
-  , 'june_G' : 'june_G.mp3'
-  , 'june_A1' : 'june_A1.mp3'
-  , 'reverb1' : '960-BigEmptyChurch.mp3'
-  , 'reverse_reverb' : 'H3000-ReverseGate.mp3'
-  , 'heartbeat':'newsamples/heartbeat0.wav'};
+  'chatter':'./sound/chatter_amp.mp3' // chatter that starts in the thrid page.
+  , 'gong' : './sound/indonesian_gong.wav' // click sound from the 2nd page.
+  , 'tick1' : './sound/tick1.wav' // click sound from the 2nd page.
+  , 'ir1' : './sound/ir1.wav' // reverb impulse response.
+  , 'sus1' : './sound/sus_note.wav' // another reberb response
+  , 'piano1': './sound/piano_note1_f_sharp.wav' // this is not used
+  , 'reversegate' :'./sound/H3000-ReverseGate.mp3'
+  ,'pianoloop2':'./sound/pianoloop2.wav'
+  ,'nnote1':'./sound/nnote1.wav'
+  ,'pause1':'./sound/note_fio.wav'
+  ,'pause2':'./sound/nnote1.wav'
+  ,'click':'./sound/click.wav'
+  ,'woodangtang':'./sound/woodangtang.wav'
+  , 'june_A' : './sound/june_A.mp3'
+  , 'june_B' : './sound/june_B.mp3'
+  , 'june_C' : './sound/june_C.mp3'
+  , 'june_D' : './sound/june_D.mp3'
+  , 'june_E' : './sound/june_E.mp3'
+  , 'june_F' : './sound/june_F.mp3'
+  , 'june_G' : './sound/june_G.mp3'
+  , 'june_A1' : './sound/june_A1.mp3'
+  , 'reverb1' : './sound/960-BigEmptyChurch.mp3'
+  , 'reverse_reverb' : './sound/H3000-ReverseGate.mp3'
+  , 'heartbeat':'./sound/heartbeat0.wav'};
 
 // currently y o u is colored white (based on volume )
 var coloredStr = [0,
